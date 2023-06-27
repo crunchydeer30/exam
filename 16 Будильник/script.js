d8eaeab9cce0cef13c84066653249ffbf1dd3cbf
Farruh4eg <114315540+Farruh4eg@users.noreply.github.com>
2023-06-27 15:49:47 +0300

    add task16

---
+++ 16 Будильник/script.js
@@ -0,0 +1,34 @@
+let alarmSound = document.getElementById('alarm-sound');
+
+function setAlarm() {
+ let hour = document.getElementById('hour').value;
+ let minute = document.getElementById('minute').value;
+ let second = document.getElementById('second').value;
+
+ if (hour == '' || minute == '' || second == '') {
+  alert('Введите время для сигнала');
+  return;
+ }
+
+ let now = new Date();
+ let alarmTime = new Date();
+ alarmTime.setHours(hour);
+ alarmTime.setMinutes(minute);
+ alarmTime.setSeconds(second);
+
+ let timeToAlarm = alarmTime - now;
+
+ if (timeToAlarm < 0) {
+  alert('Время для сигнала уже прошло');
+  return;
+ }
+
+ setTimeout(function() {
+  alarmSound.play();
+ }, timeToAlarm);
+}
+
+function stopAlarm() {
+ alarmSound.pause();
+ alarmSound.currentTime = 0;
+}
