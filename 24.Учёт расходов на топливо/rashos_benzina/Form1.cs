00b09f20db85dc900b9acace0f6504dfdb7578bf
SSAW13 <132646522+SSAW13@users.noreply.github.com>
2023-06-28 23:35:21 +0300

    Add files via upload

---
+++ 24.Учёт расходов на топливо/rashos_benzina/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rashos_benzina
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float p = Convert.ToInt32(textBox1.Text);
+            float r = Convert.ToInt32(textBox2.Text);
+            float S = Convert.ToInt32(textBox3.Text);
+            if (p >= 0 && r >= 0 && S >= 0)
+            {
+                float x = r / 100;//кол-во литров на 1км
+                float xp = x * p;//цена за 1 км
+                float rez = xp * S;//цена за весь путь
+
+                label5.Text = x + " кол-во литров топлива на 1км";
+                label6.Text = xp + " стоимость 1км";
+                label7.Text = rez + " цена за весь путь";
+            }
+            else
+            {
+                label5.Text = "Значение не может быть меньше или равно нулю!";
+                label6.Text = "Значение не может быть меньше или равно нулю!";
+                label7.Text = "Значение не может быть меньше или равно нулю!";
+            }
+        }
+
+    }
+}
---
+++ 24.Учёт расходов на топливо/rashos_benzina/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("rashos_benzina")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rashos_benzina")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("81dd4cd3-bb82-41ba-a868-5ae2a0797b60")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
