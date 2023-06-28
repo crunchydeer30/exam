04a526a5c6b99e1664b6b94002dd45ae062ae2f3
Farruh4eg <114315540+Farruh4eg@users.noreply.github.com>
2023-06-29 02:30:26 +0300

    add cypress template

---
+++ Cypress template/README.md
@@ -0,0 +1,8 @@
+
+# Установка
+
+    npm i cypress --save-dev
+	
+# Запуск
+
+    npx cypress run --headed --browser firefox
---
+++ Cypress template/cypress/e2e/sample.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Проверка кнопки "Старт"', () => {
+    it('Проверка нажатия кнопки', () => {
+        cy.visit('http://127.0.0.1:3002');
+        cy.get('button').contains('Старт').click();
+        cy.get('h1').should('not.contain', '00:00:00');
+    })
+});
+
+describe('Проверка кнопки "Сброс"', () => {
+    it('Проверка нажатия кнопки', () => {
+        cy.visit('http://127.0.0.1:3002');
+        cy.get('button').contains('Сброс').click();
+        cy.get('h1').should('contain', '00:00:00');
+    })
+})
