6bad979c40ea0f80f9421064d5aff7522e509eee
unknown <crunchydeer30@gmail.com>
2023-06-28 21:49:00 +0300

    add app22

---
+++ 22 Контакты/ПриложениеКонтакты/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace ПриложениеКонтакты
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.telephone = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addContactBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Название контакта";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Номер";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(202, 33);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(241, 26);
+            this.name.TabIndex = 2;
+            // 
+            // telephone
+            // 
+            this.telephone.Location = new System.Drawing.Point(145, 84);
+            this.telephone.Name = "telephone";
+            this.telephone.Size = new System.Drawing.Size(298, 26);
+            this.telephone.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 272);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(412, 496);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // addContactBtn
+            // 
+            this.addContactBtn.Location = new System.Drawing.Point(31, 145);
+            this.addContactBtn.Name = "addContactBtn";
+            this.addContactBtn.Size = new System.Drawing.Size(412, 43);
+            this.addContactBtn.TabIndex = 5;
+            this.addContactBtn.Text = "Добавить";
+            this.addContactBtn.UseVisualStyleBackColor = true;
+            this.addContactBtn.Click += new System.EventHandler(this.addContactBut_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(31, 208);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(412, 43);
+            this.saveBtn.TabIndex = 6;
+            this.saveBtn.Text = "Сохранить";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 797);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.addContactBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.telephone);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox telephone;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addContactBtn;
+        private System.Windows.Forms.Button saveBtn;
+    }
+}
+
---
+++ 22 Контакты/ПриложениеКонтакты/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПриложениеКонтакты
+{
+    public partial class Form1 : Form
+    {
+        public List<Contact> contacts = new List<Contact>();
+        public List<String> newContacts = new List<String>();
+        string path = "C:/Users/Владислава/Desktop/Учеба/МодульныйЭкзамен/ПриложениеКонтакты/contacts.txt";
+        public class Contact
+        {
+            public string name;
+            public string telephone;
+
+            public Contact(string nname, string ntelephone)
+            {
+                name = nname;
+                telephone = ntelephone;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public void addContactBut_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Add(name.Text, telephone.Text);
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+            StreamReader file = new StreamReader(path);
+            List<string> rows = file.ReadLine().Split(' ').ToList();
+            file.Close();
+            string nme = "";
+            string tlphone = "";
+            int a = 0;
+            foreach (string row in rows)
+            {
+                if (a % 2 == 0)
+                {
+                    nme = row;
+                } else
+                {
+                    tlphone = row;
+                    contacts.Add(new Contact(nme, tlphone));
+                }
+                a++;
+            }
+
+            dataGridView1.Columns.Add("Column1", "Имя");
+            dataGridView1.Columns.Add("Column2", "Телефон");
+
+            foreach (var i in contacts)
+            {
+                dataGridView1.Rows.Add(i.name, i.telephone);
+            }
+        }
+
+        public void saveBtnClick(object sender, EventArgs e)
+        {
+            newContacts.Clear();
+            for (int v = 0; v < dataGridView1.Rows.Count - 1; v++)
+            {
+                newContacts.Add(dataGridView1[0,v].Value.ToString());
+                newContacts.Add(dataGridView1[1, v].Value.ToString());
+            }
+            string writingText = "";
+            //
+            foreach (string str in newContacts)
+            {
+                writingText += str + " ";
+            }
+            //
+
+            // полная перезапись файла 
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(writingText);
+            }
+        }
+    }
+}
